b7f7e754d56e3af4c6542feb11a76c8fa29e51a2
blacksmitj <andri.andreas.panjaitan@gmail.com>
2022-09-19 21:08:22 +0700

    adding Seeder

---
+++ database/seeders/CampTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Camp;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CampTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $camps = [
+            [
+                'title' => 'Gila Belajar',
+                'slug' => 'gila-belajar',
+                'price' => 280,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'Baru Mulai',
+                'slug' => 'baru-mulai',
+                'price' => 140,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ];
+
+        // foreach ($camps as $key => $camp) {
+        //     Camp::create($camp);
+        // }
+
+        Camp::insert($camps);
+    }
+}
